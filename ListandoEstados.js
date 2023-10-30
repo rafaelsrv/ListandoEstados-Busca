296b3de0195890111f9f52dd5a26966d97b26982
Rafael <rafael-srv@hotmail.com>
2023-10-29 23:26:12 -0300

    Implementando busca

--- ListandoEstados.js
+++ ListandoEstados.js
@@ -4,11 +4,26 @@ function listar () {
     const url  = "dados/estados.json"
     axios(url).then(dados =>{
        const listagem = dados.data.reduce(
-        (html ,estado) => html + `<li>${estado.nome}</li>`, ''
+        (html ,estado) => html + `<li class="animal">${estado.nome}</li>`, ''
         
         
     )
     document.body.insertAdjacentHTML('beforeend', `<ul>${listagem}</ul>`)
 
     })
+}
+function search (){
+let input = document.getElementById('searchInput').value
+input = input.toLowerCase()
+console.log(input)
+let x = document.getElementsByClassName('animal')
+
+for (i = 0; i < x.length; i++) { 
+    if (x[i].innerHTML.toLowerCase().includes(input)) {
+        x[i].style.display="none";
+    }
+    else {
+        x[i].style.display="list-item";                 
+    }
+}
 }
